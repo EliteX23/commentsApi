b675fbd7056eef3227fe50f1be9cb607e5dfed9b
Alex <elitex25@gmail.com>
2020-01-18 01:40:05 +0300

    Интерфейс для репозитория

---
+++ Interfaces/ICommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetCommentList();
+        Task<Comment> GetComment(string id);
+        Task Create(Comment item);
+        Task<ReplaceOneResult> Update(string id, Comment item);
+        Task<DeleteResult> Delete(string id);
+
+    }
+}
