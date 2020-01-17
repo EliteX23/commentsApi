32c220935461ffa6484d2c3b53342a46e2f54bfa
Alex <elitex25@gmail.com>
2020-01-18 01:32:01 +0300

    Доменная модель

---
+++ Core/Comment.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Core
+{
+    public class Comment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string Author { get; set; }
+        public string Topic { get; set; }
+
+    }
+}
