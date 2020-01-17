a8bbe417ffb0eb6712cb6cdfb4f273711d1ae2bc
Alex <elitex25@gmail.com>
2020-01-18 01:51:27 +0300

    Интерфейс сервиса

---
+++ Services.Interfaces/IComment.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IComment
+    {
+        Task<IEnumerable<Comment>> GetList();
+        Task<Comment> GetComment(string id);
+        Task Save(Comment comment);
+        Task Update(string id, Comment item);
+        Task Delete(string id);
+    }
+}
