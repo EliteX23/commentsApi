f062a1d57dad8fca6b2d1288b8b17318f899b30d
Alex <elitex25@gmail.com>
2020-01-18 22:58:29 +0300

    Методы PUT и Delete

--- commentsApi/Controllers/CommentController.cs
+++ commentsApi/Controllers/CommentController.cs
@@ -43,5 +43,35 @@ namespace commentsApi.Controllers
 
             return CreatedAtRoute("GetComment", new { id = comment.Id.ToString() }, comment);
         }
+       
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var comment = _commentService.GetComment(id);
+            var result = comment.Result;
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            _commentService.Delete(result.Id);
+
+            return NoContent();
+        }
+        
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Comment commentIn)
+        {
+            var book = _commentService.GetComment(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _commentService.Update(id, commentIn);
+
+            return NoContent();
+        }
     }
 }
